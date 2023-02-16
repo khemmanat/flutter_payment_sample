ab62266a486a466888c47878e56abae9b9a2e772
KhemmanatTo <khemmanat2012@gmail.com>
2023-02-16 15:28:26 +0700

    update base_view

--- lib/core/widgets/base_view.dart
+++ lib/core/widgets/base_view.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
 
 class BaseView extends StatelessWidget {
-  const BaseView({Key? key, this.appBar, required this.child})
-      : super(key: key);
+  const BaseView({
+    Key? key,
+    this.appBar,
+    required this.child,
+    this.bottomNavigationBar,
+    this.floatingActionWidget,
+    this.drawer,
+  }) : super(key: key);
 
   final PreferredSizeWidget? appBar;
+  final Widget? drawer;
   final Widget child;
+  final Widget? bottomNavigationBar;
+  final Widget? floatingActionWidget;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar,
+      drawer: drawer,
       body: SafeArea(
         child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: child),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: child,
+        ),
       ),
+      bottomNavigationBar: bottomNavigationBar,
+      floatingActionButton: floatingActionWidget,
     );
   }
 }
