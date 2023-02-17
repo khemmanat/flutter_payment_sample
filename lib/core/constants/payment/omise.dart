a157d1fa1b0ec67e0778629a20d1e935b720353a
KhemmanatTo <khemmanat2012@gmail.com>
2023-02-17 22:18:03 +0700

    add omise services

--- lib/core/constants/payment/omise.dart
+++ lib/core/constants/payment/omise.dart
@@ -1,3 +1,7 @@
 class OmiseConstant {
+  // real test key
   static const String publicKey = "pkey_test_5o7gyk3jag449lw7lgq";
+  static const String secretKey = "skey_test_5o7gyk3j9m5tc6mlvw6";
+  // fake test key
+  // static const String publicKey = "pkey_test_5o7gyk3jag449lw7lgq";
 }
--- lib/features/payment/pages/omise/omise_page.dart
+++ lib/features/payment/pages/omise/omise_page.dart
@@ -12,6 +12,7 @@ class OmisePaymentPage extends StatefulWidget {
 
 class _OmisePaymentPageState extends State<OmisePaymentPage> {
   OmiseFlutter omise = OmiseFlutter(OmiseConstant.publicKey);
+  TextEditingController omiseToken = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -22,10 +23,64 @@ class _OmisePaymentPageState extends State<OmisePaymentPage> {
           children: [
             ElevatedButton(
                 onPressed: () {
-                  Navigator.pop(context);
+                  createToken();
                 },
-                child: const Text('Back'))
+                child: Text('Create Token')),
+            ElevatedButton(
+                onPressed: () {
+                  createSource();
+                },
+                child: Text('Create Source')),
+            ElevatedButton(
+                onPressed: () {
+                  retrieveCapability();
+                },
+                child: Text('Retrieve Capability')),
+            ElevatedButton(
+                onPressed: () {
+                  charge();
+                },
+                child: Text('Charge'))
+            // ElevatedButton(
+            //     onPressed: () {
+            //       Navigator.pop(context);
+            //     },
+            //     child: const Text('Back'))
           ],
         ));
+  }
+
+  createToken() async {
+    final token = await omise.token
+        .create('JOHN DOE', '4242424242424242', '10', '2024', '123');
+    setState(() {
+      omiseToken.text = token.id!;
+    });
+    // print('TOKEN: ${token.id}');
+  }
+
+  createSource() async {
+    final source =
+        await omise.source.create(10000, 'thb', 'internet_banking_bay');
+    print('SOURCE ID: ${source.id}');
+  }
+
+  retrieveCapability() async {
+    final capability = await omise.capability.retrieve();
+    capability.paymentMethods?.forEach((element) {
+      print(element.name);
+    });
+  }
+
+  charge() async {
+    final charge = await omise.client.post('api.omise.co', [
+      '/charges'
+    ], data: {
+      'amount': 10000,
+      'currency': 'thb',
+      'source': 'src_test_5uu37nqrpwhsa13ui2z',
+      'return_uri': 'http://www.example.com'
+    });
+    print('CHARGE: $charge');
   }
 }
